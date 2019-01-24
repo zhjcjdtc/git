bcb26302afc09da744560bae397ec3b9f1bfe35f
zhanghongjie <zhjcjdtc@163.com>
2019-01-24 23:09:05 +0800

    19.1.24

---
+++ awesome-python3-webapp/www/static/orm.py
@@ -0,0 +1,17 @@
+async def create_pool(**kw):
+	logging.info('create datebase connection pool')
+	global __pool
+	__pool = await aiomysql.create_pool(
+		host = kw.get('host','localhost'),
+		port = kw.get('port',3306),
+		user = kw['user'],
+		password = kw['password'],
+		db = kw['db'],
+		charset = kw.get('charset','utf8'),
+		autocommit = kw.get('autocommit',True),
+		maxsize = kw.get('maxsize',10),
+		minsize = kw.get('minsize',1),
+	)
+
+async def select(
+	
